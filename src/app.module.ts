268ce4b7b63e0e43f9be63a713cca3ef9128f33f
Raymond <73725736+raymondanythings@users.noreply.github.com>
2023-01-27 16:56:57 +0900

    🎨feat : 글로벌 Logger 추가, env 셋업
    - env : test, development, production으로 관리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { LoggerMiddleware } from './logger/logger.middleware'
+import joi from 'joi'
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'development'
+          ? '.env.development'
+          : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      validationSchema: joi.object({
+        PORT: joi.string().required(),
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*')
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule } from '@nestjs/swagger'
+import { DocumentBuilder } from '@nestjs/swagger/dist'
+import { AppModule } from './app.module'
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api/v1')
+  const config = new DocumentBuilder()
+    .setTitle('Plug Api')
+    .setVersion('0.0.1')
+    .setDescription('Plug Webrtc socket server')
+    .build()
+  const documnet = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documnet)
+  await app.listen(PORT)
+  console.log(`app listen on port : ${PORT}`)
 }
-bootstrap();
+bootstrap()
